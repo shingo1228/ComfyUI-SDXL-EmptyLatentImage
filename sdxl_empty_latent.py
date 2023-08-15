d415b5bd4f19fb9ee804b9e94d26d3010bc3c541
Shingo.T <fow1228@gmail.com>
2023-08-15 16:24:03 +0900

    Support additional json file

--- sdxl_empty_latent.py
+++ sdxl_empty_latent.py
@@ -6,41 +6,58 @@ MAX_RESOLUTION = 8192
 DEBUG = False
 
 
-def extract_resolutions_with_key(file_path):
-    with open(file_path, "r") as file:
-        json_data = json.load(file)
+def get_all_json_files(directory):
+    return [
+        os.path.join(directory, file)
+        for file in os.listdir(directory)
+        if file.endswith(".json") and os.path.isfile(os.path.join(directory, file))
+    ]
+
+
+def load_resolutions_from_directory(directory):
+    json_files = get_all_json_files(directory)
+
+    if DEBUG:
+        for json_file in json_files:
+            print(f"json_file:{json_file}")
 
     resolutions_dict = {}
-    for item in json_data:
-        width = item["width"]
-        height = item["height"]
-        aspect_ratio = "{:.2f}".format(round(width / height, 2))
-        key = f"{width} x {height} ({aspect_ratio})"
-        resolutions_dict[key] = {"width": width, "height": height}
+    for json_file in json_files:
+        with open(json_file, "r") as file:
+            json_data = json.load(file)
+
+        for item in json_data:
+            width = item["width"]
+            height = item["height"]
+            aspect_ratio = "{:.2f}".format(round(width / height, 2))
+            key = f"{width} x {height} ({aspect_ratio})"
+            resolutions_dict[key] = {"width": width, "height": height}
 
     return resolutions_dict
 
 
 class SdxlEmptyLatentImage:
+    resolution_dictionaly = None
+
     def __init__(self, device="cpu"):
         self.device = device
 
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        json_file_name = os.path.join(current_dir, "sdxl_resolution_set.json")
-        self.resolutions = extract_resolutions_with_key(json_file_name)
+        if self.resolution_dictionaly is None:
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            self.resolution_dictionaly = load_resolutions_from_directory(current_dir)
 
         if DEBUG:
-            for key, item in self.resolutions.items():
+            for key, item in self.resolution_dictionaly.items():
                 print(f"key:{key} w:{item['width']} h:{item['height']}")
 
     @classmethod
     def INPUT_TYPES(s):
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        json_file_name = os.path.join(current_dir, "sdxl_resolution_set.json")
+        if s.resolution_dictionaly is None:
+            s.resolution_dictionaly = load_resolutions_from_directory(current_dir)
 
-        resolutions = extract_resolutions_with_key(json_file_name)
         res_list = []
-        for key in resolutions:
+        for key in s.resolution_dictionaly:
             res_list.append(key)
 
         if DEBUG:
@@ -61,14 +78,14 @@ class SdxlEmptyLatentImage:
     def generate(self, resolution, batch_size=1):
         if DEBUG:
             print(f"res_key:{resolution}")
-            print(f"item:{self.resolutions[resolution]}")
+            print(f"item:{self.resolution_dictionaly[resolution]}")
 
         latent = torch.zeros(
             [
                 batch_size,
                 4,
-                self.resolutions[resolution]["height"] // 8,
-                self.resolutions[resolution]["width"] // 8,
+                self.resolution_dictionaly[resolution]["height"] // 8,
+                self.resolution_dictionaly[resolution]["width"] // 8,
             ]
         )
         return ({"samples": latent},)
